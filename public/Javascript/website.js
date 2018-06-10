76372db7b035b0254fbdae7849efb4cb9fb21c85
WaynePhung <waynewphung@aol.com>
2018-06-10 03:58:26 -0700

    Reverting to basic bootstrap columns for now. Toggle button works.

--- public/Javascript/website.js
+++ public/Javascript/website.js
@@ -1,7 +1,7 @@
 "use strict";
 
 $(document).ready(function () {
-    let sidebar = $('.sidebar');
+    let sidebar = $('#sidebar');
     let sideButton = $('.togSide');
     sideButton.click( function(){
         sidebar.toggleClass('collapsed');
