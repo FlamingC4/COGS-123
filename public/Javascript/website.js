c6ed041cd88799dbf3bcefb83c403f3606027ed6
WaynePhung <waynewphung@aol.com>
2018-06-10 03:05:23 -0700

    This is some new code to test for a collapsible vertical sidebar and Bootstrap implementation.

---
+++ public/Javascript/website.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(document).ready(function () {
+    let sidebar = $('.sidebar');
+    let sideButton = $('.togSide');
+    sideButton.click( function(){
+        sidebar.toggleClass('collapsed');
+    });
+});
